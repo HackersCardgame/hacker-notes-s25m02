564e26542f274e1afcd208ac210eb0db95222a84
Your Name <you@example.com>
2025-04-16 09:36:05 +0200

    ...

--- tools/mpg_123.sh
+++ tools/mpg_123.sh
@@ -13,7 +13,11 @@
 
 #find /media/marc/77*/Music.merged/ -name "*20.03.2021*" -type f -print0 | shuf -z | xargs -0  mpg123 -v -k 159000  
 
-find /media/marc/77*/Music.merged/ -name "*Zonne*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
+#find /media/marc/77*/Music.merged/ -name "*Zonne*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
+#find /media/marc/77*/Music.merged/ -name "*Finger*" -type d -print0 -exec ls | shuf -z | xargs -0  mpg123 -v 
+find /media/marc/77*/Music.merged/ -name "*Finger*" -type d -print0 -exec find '{}'  -print0 \; | shuf -z | xargs -0  mpg123 -v 
+
+
 
 
 
