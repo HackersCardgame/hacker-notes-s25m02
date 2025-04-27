f8f8a41de7ee1e6010d7a69f35b321d7489f1003
Your Name <you@example.com>
2025-04-27 16:45:44 +0200

    ...

--- tools/mpg_123.sh
+++ tools/mpg_123.sh
@@ -6,14 +6,14 @@
 
 
 
-#find /media/marc/77*/Music.merged/ -name "*cl*" -type f -print0 | shuf -z | xargs -0  mpg123  
+find /media/marc/77*/Music.merged/ -name "*cl*" -type f -print0 | shuf -z | xargs -0  mpg123  
 #find /media/marc/77*/Music.merged/ -name "*282*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
 #find /media/marc/77*/Music.merged/ -name "*inse*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
 #find /media/marc/77*/Music.merged/ -name "*es*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
 
 #find /media/marc/77*/Music.merged/ -name "*20.03.2021*" -type f -print0 | shuf -z | xargs -0  mpg123 -v -k 159000  
 
-find /media/marc/77*/Music.merged/ -name "*Zonne*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
+#find /media/marc/77*/Music.merged/ -name "*Zonne*" -type f -print0 | shuf -z | xargs -0  mpg123 -v 
 #find /media/marc/77*/Music.merged/ -name "*Finger*" -type d -print0 -exec ls | shuf -z | xargs -0  mpg123 -v 
 #find /media/marc/77*/Music.merged/ -name "*Finger*" -type d -print0 -exec find '{}'  -print0 \; | shuf -z | xargs -0  mpg123 -v 
 
